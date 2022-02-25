df53374b0ad60e650fdc4652a34dc4033aec38c3
Ali Ghafouri Pourkermani <ghafouri@login4.frontera.tacc.utexas.edu>
2022-02-24 21:12:58 -0600

    up

---
+++ env_source.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/work2/07544/ghafouri/frontera/gits/claire/deps/lib/lib:$LD_LIBRARY_PATH
+export PETSC_DIR=/work2/07544/ghafouri/frontera/gits/claire/deps/lib
+export NIFTI_DIR=/work2/07544/ghafouri/frontera/gits/claire/deps/lib
